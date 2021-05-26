30be433dd6d2aeb034cbb1e62de19c4ad5e7e6f5
nitinchamoli9425 <nitinchamoli9425@hotmail.com>
2021-05-26 02:42:20 +0200

    final commit

---
+++ Assets/Script/Panel.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using UnityMQTTROS2;
+
+namespace UnityMQTTROS2.PanelChild
+{
+    public class Panel : UnityMQTT
+    {
+        // Start is called before the first frame update
+        public GameObject PanelOpener;
+
+        [Header("Panel configuration to connect to MQTT")]
+        public Button startButton;
+        public Button stop;
+        public InputField brokerAddress;
+        public InputField brokerPortNumber;
+        public InputField topicsPublish;
+        public InputField topicsSubscribe;
+        public InputField qos;
+        public InputField messagePublished;
+        public InputField topicsUnsubscribe;
+        public Button erase;
+        public string[] publishTopic, msgPublish, subscribeTopic, unsubscribeTopic;
+        // private MQTTClient unityMQTT;
+
+        private List<string> eventMessages = new List<string>();
+
+        //toogle button (MQTT) to open the panel
+        public void openPanel()
+        {
+            if (PanelOpener != null)
+            {
+                bool isActive = PanelOpener.activeSelf;
+                PanelOpener.SetActive(!isActive);
+            }
+        }
+
+        public void InputBrokerAddress(string brokerAddressMQTT)
+        {
+            if (brokerAddress)
+            {
+                this.brokerAddressMQTT = brokerAddressMQTT;
+                //Debug.Log("from input broker add" + brokerAddressMQTT);
+            }
+        }
+
+        public void InputPortNumber(string portNumber)
+        {
+            if (brokerPortNumber)
+            {
+                int.TryParse(portNumber, out this.portNumber);
+            }
+        }
+
+        public void InputPublishTopics(string publishTopics)
+        {
+            if (topicsPublish != null)
+            {
+                this.publishTopics = publishTopics;
+                publishTopic = publishTopics.Split(',');
+                Debug.LogFormat("publish topic is  " + this.publishTopics + "  publishtopic  " + publishTopic);
+            }
+        }
+
+        public void InputMessagePublish(string messagePublish)
+        {
+            if (messagePublished != null)
+            {
+                this.messagePublish = messagePublish;
+                msgPublish = messagePublish.Split(',');
+            }
+        }
+
+        public void InputQOS(string qoss)
+        { 
+            if (qos != null)
+                byte.TryParse(qoss, out this.qoss);
+        }
+
+
+        public void InputSubcribeTopics(string subscribeTopics)
+        {
+            if (topicsSubscribe != null)
+            {
+                this.subscribeTopics = subscribeTopics;
+                subscribeTopic = subscribeTopics.Split(',');
+               // Debug.Log("i am at subrcibe func");
+            }
+            //if (qos != null)
+            //    byte.TryParse(qoss, out this.qoss);
+        }
+
+        public void InputUnsubscribeTopics(string unsubscribeTopics)
+        {
+            if (topicsUnsubscribe != null)
+            {
+                this.unsubscribeTopics = unsubscribeTopics;
+                unsubscribeTopic = unsubscribeTopics.Split(',');
+            }
+        }
+
+        protected override void WhenConnected()
+        {
+            Debug.LogFormat("Connected to the broker address \"" + brokerAddressMQTT + "\" at port \"" + portNumber + "\" \n");
+            base.WhenConnected();
+           /* PublishTopics();
+            SubscribeTopics();
+            UnsubscribeTopics(); */
+        }
+
+        protected override void PublishTopics()
+        {
+             Debug.Log("Publish topic function \n");
+             for (int i = 0; i < publishTopic.Length; i++)
+             {
+                 Debug.Log("Publish topic function loop" + i + "\n");
+
+                 unityMQTT.Publish(publishTopic[i], System.Text.Encoding.UTF8.GetBytes(msgPublish[i]), qoss, false);
+                 Debug.Log("Test Message: \"" + msgPublish[i] + "\" is published");
+             } 
+            Debug.LogFormat("Publish topic function \n");
+           // unityMQTT.Publish("nitin", System.Text.Encoding.UTF8.GetBytes("Hello from Unity!"), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
+        }
+
+        protected override void SubscribeTopics()
+        {
+            Debug.LogFormat("Subscribe topic function \n");
+            //do not forget to check qos later
+            unityMQTT.Subscribe(subscribeTopic, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+            for (int i = 0; i < subscribeTopic.Length; i++)
+            {
+                Debug.Log("Topics \"" + subscribeTopic[i] + "\" subscribed");
+            }
+        }
+
+        protected override void UnsubscribeTopics()
+        {
+
+            unityMQTT.Unsubscribe(unsubscribeTopic);
+            for (int i = 0; i < unsubscribeTopic.Length; i++)
+            {
+                Debug.Log("Topic(s) \"" + unsubscribeTopic[i] + "\" unsubscribed");
+            }
+        }
+
+        private void UIInteract()
+        {
+            if (unityMQTT == null)
+            {
+                if (startButton != null)
+                {
+                    startButton.interactable = true;
+                    stop.interactable = false;
+                }
+            }
+            else
+            {
+                if (stop != null)
+                {
+                    stop.interactable = unityMQTT.IsConnected;
+                }
+                if (startButton != null)
+                {
+                    startButton.interactable = !unityMQTT.IsConnected;
+                }
+            }
+            if (brokerAddress != null && startButton != null)
+            {
+                brokerAddress.interactable = startButton.interactable;
+                brokerAddress.text = brokerAddressMQTT;
+            }
+            if (brokerPortNumber != null && startButton != null)
+            {
+                brokerPortNumber.interactable = startButton.interactable;
+                brokerPortNumber.text = portNumber.ToString();
+            }
+            if (qos != null && startButton != null)
+            {
+                qos.interactable = startButton.interactable;
+                qos.text = qoss.ToString();
+            }
+            if (topicsPublish != null && startButton != null)
+            {
+                topicsPublish.interactable = true;
+                topicsPublish.text = publishTopics;
+            }
+            if (messagePublished != null && startButton != null)
+            {
+                messagePublished.interactable = startButton.interactable;
+                messagePublished.text = messagePublish;
+            }
+            if (topicsSubscribe != null && startButton != null)
+            {
+                topicsSubscribe.interactable = startButton.interactable;
+                topicsSubscribe.text = subscribeTopics;
+            }
+            if (topicsUnsubscribe != null && startButton != null)
+            {
+                topicsUnsubscribe.interactable = startButton.interactable;
+                topicsUnsubscribe.text = unsubscribeTopics;
+            }
+            if (erase != null && startButton != null)
+            {
+                erase.interactable = startButton.interactable;
+            }
+        }
+
+        protected override void DecodeMessage(string topic, byte[] message)
+        {
+            string msg = System.Text.Encoding.UTF8.GetString(message);
+            Debug.Log("Received: " + msg);
+            StoreMessage(msg);
+        }
+
+        private void StoreMessage(string eventMsg)
+        {
+            eventMessages.Add(eventMsg);
+        }
+
+        private void ProcessMessage(string msg)
+        {
+            Debug.Log("Received: " + msg);
+        }
+
+        protected override void Update()
+        {
+            base.Update(); // call ProcessMqttEvents()
+            if (eventMessages.Count > 0)
+            {
+                foreach (string msg in eventMessages)
+                {
+                    ProcessMessage(msg);
+                }
+                eventMessages.Clear();
+            }
+        }
+
+        private void OnDestroy()
+        {
+            Disconnect();
+        }
+        //private void onvalidate()
+        //{
+           
+        //}
+    }
+}
---
+++ Assets/Script/UnityMQTT.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace UnityMQTTROS2
+{
+    public class UnityMQTT : MonoBehaviour
+    {
+        //define variables whose values are obtained from UI
+        public string brokerAddressMQTT;
+        public int portNumber;
+        public bool isEncrypted = false;
+        public int connectionDelay = 500;
+        public string publishTopics, subscribeTopics, unsubscribeTopics, messagePublish;
+        public byte qoss;
+        public int timeout = MqttSettings.MQTT_CONNECT_TIMEOUT;
+        //Note: later add username passwword
+
+        //create instance
+        protected MqttClient unityMQTT;
+
+        private List<MqttMsgPublishEventArgs> messageQueue1 = new List<MqttMsgPublishEventArgs>(); 
+        private List<MqttMsgPublishEventArgs> messageQueue2 = new List<MqttMsgPublishEventArgs>();
+        private List<MqttMsgPublishEventArgs> frontMessageQueue = null;
+        private List<MqttMsgPublishEventArgs> backMessageQueue = null;
+        private bool mqttClientConnectionClosed = false;
+        private bool mqttClientConnected = false;
+
+        //Event action when connection is successful
+        public event Action ConnectionSucceeded;
+
+        //Event action when connection is unsuccessful
+        public event Action ConnectionFailed;
+
+        public virtual void Connect()
+        {
+            if (unityMQTT == null || !unityMQTT.IsConnected)
+            {
+               // Debug.Log("this works!! from input broker add" + this.brokerAddressMQTT  + " this:--" + brokerAddressMQTT);
+                StartCoroutine(DoConnect());
+            }
+        }
+
+        public virtual void Disconnect()
+        {
+            if (unityMQTT != null)
+            {
+                StartCoroutine(DoDisconnect());
+            }
+        }
+
+        protected virtual void WhenConnected()
+        {
+            PublishTopics();
+            SubscribeTopics();
+
+            if (ConnectionSucceeded != null)
+            {
+                ConnectionSucceeded();
+            }
+        }
+
+        protected virtual void WhenConnectionFails(string errorMessage)
+        {
+            Debug.LogWarning("Connection failed." + errorMessage);
+            if (ConnectionFailed != null)
+            {
+                ConnectionFailed();
+            }
+        }
+
+        protected virtual void PublishTopics()
+        {
+            //Debug.Log("hi from Publish base class function");
+        }
+
+        protected virtual void SubscribeTopics()
+        {
+            //Debug.Log("hi from subcribe base class function");
+        }
+
+        protected virtual void UnsubscribeTopics()
+        {
+
+        }
+
+        protected virtual void OnApplicationQuit()
+        {
+            CloseConnection();
+        }
+
+        protected virtual void Awake()
+        {
+            frontMessageQueue = messageQueue1;
+            backMessageQueue = messageQueue2;
+        }
+        //        client.publish();
+        protected virtual void start()
+        {
+        }
+        protected virtual void DecodeMessage(string topic, byte[] message)
+        {
+        }
+
+        protected virtual void OnDisconnected()
+        {
+            Debug.Log("Disconnected.");
+        }
+
+        protected virtual void OnConnectionLost()
+        {
+            Debug.LogWarning("CONNECTION LOST!");
+        }
+
+        protected virtual void Update()
+        {
+            ProcessMqttEvents();
+        }
+
+        protected virtual void ProcessMqttEvents()
+        {
+            // process messages in the main queue
+            SwapMqttMessageQueues();
+            ProcessMqttMessageBackgroundQueue();
+            // process messages income in the meanwhile
+            SwapMqttMessageQueues();
+            ProcessMqttMessageBackgroundQueue();
+
+            if (mqttClientConnectionClosed)
+            {
+                mqttClientConnectionClosed = false;
+                OnConnectionLost();
+            }
+        }
+
+        private void ProcessMqttMessageBackgroundQueue()
+        {
+            foreach (MqttMsgPublishEventArgs msg in backMessageQueue)
+            {
+                DecodeMessage(msg.Topic, msg.Message);
+            }
+            backMessageQueue.Clear();
+        }
+
+        private void SwapMqttMessageQueues()
+        {
+            frontMessageQueue = frontMessageQueue == messageQueue1 ? messageQueue2 : messageQueue1;
+            backMessageQueue = backMessageQueue == messageQueue1 ? messageQueue2 : messageQueue1;
+        }
+
+        private void OnMqttMessageReceived(object sender, MqttMsgPublishEventArgs msg)
+        {
+            frontMessageQueue.Add(msg);
+        }
+
+        private void OnMqttConnectionClosed(object sender, EventArgs e)
+        {
+            // Set unexpected connection closed only if connected (avoid event handling in case of controlled disconnection)
+            mqttClientConnectionClosed = mqttClientConnected;
+            mqttClientConnected = false;
+        }
+
+        private IEnumerator DoConnect()
+        {
+            // wait for the given delay
+            yield return new WaitForSecondsRealtime(connectionDelay / 1000f);
+            // leave some time to Unity to refresh the UI
+            yield return new WaitForEndOfFrame();
+
+            // create client instance 
+            if (unityMQTT == null)
+            {
+                try
+                {
+                   // Debug.LogFormat("brioker address " + brokerAddressMQTT + " port number" + portNumber + " topicd publish " + publishTopics + " msg publish " + messagePublish);
+                    unityMQTT = new MqttClient(brokerAddressMQTT, portNumber, false, null, null, false ? MqttSslProtocols.SSLv3 : MqttSslProtocols.None);
+                }
+                catch (Exception e)
+                {
+                    unityMQTT = null;
+                    Debug.LogErrorFormat("CONNECTION FAILED! {0}", e.ToString());
+                    WhenConnectionFails(e.Message);
+                    yield break;
+                }
+            }
+            else if (unityMQTT.IsConnected)
+            {
+                yield break;
+            }
+
+            // leave some time to Unity to refresh the UI
+            yield return new WaitForEndOfFrame();  
+            yield return new WaitForEndOfFrame();
+
+            unityMQTT.Settings.TimeoutOnConnection = timeout;
+            string clientId = Guid.NewGuid().ToString();
+            try
+            {
+                unityMQTT.Connect(clientId, null, null);
+            }
+            catch (Exception e)
+            {
+                unityMQTT = null;
+                Debug.LogErrorFormat("Failed to connect to {0}:{1}:\n{2}", brokerAddressMQTT, portNumber, e.ToString());
+                WhenConnectionFails(e.Message);
+                yield break;
+            }
+            if (unityMQTT.IsConnected)
+            {
+                unityMQTT.ConnectionClosed += OnMqttConnectionClosed;
+                // register to message received 
+                unityMQTT.MqttMsgPublishReceived += OnMqttMessageReceived;
+                mqttClientConnected = true;
+                WhenConnected();
+            }
+            else
+            {
+                WhenConnectionFails("CONNECTION FAILED!");
+            }
+        }
+
+        private IEnumerator DoDisconnect()
+        {
+            yield return new WaitForEndOfFrame();
+            CloseConnection();
+            OnDisconnected();
+        }
+
+        private void CloseConnection()
+        {
+            mqttClientConnected = false;
+            if (unityMQTT != null)
+            {
+                if (unityMQTT.IsConnected)
+                {
+                    UnsubscribeTopics();
+                    unityMQTT.Disconnect();
+                }
+                unityMQTT.MqttMsgPublishReceived -= OnMqttMessageReceived;
+                unityMQTT.ConnectionClosed -= OnMqttConnectionClosed;
+                unityMQTT = null;
+            }
+        }
+    }
+}
